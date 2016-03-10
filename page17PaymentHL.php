9e6b84474d3e27463652910eee749973b740fbe2
Shelly <lihanzi1@gmail.com>
2016-03-09 23:53:02 -0800

    payment done

--- page17PaymentHL.php
+++ page17PaymentHL.php
@@ -11,8 +11,6 @@
     <!---css & js-->
     <link rel="stylesheet" type="text/css" href="Css/Page1HomePageStyle.css">
     <link rel="stylesheet" type="text/css" href="Css/commonStyle.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
-    <script src="jquery.payment.js"></script>
 	<script src="Js/jquery-1.7.2.min.js"></script>
 	<script type="text/javascript" src="Js/pptBox.js"></script>
     
@@ -24,28 +22,6 @@
 	<link rel="shortcut icon" href="Image/logoOnly.png" >
     <link rel="icon" href="Image/logoOnly.png">
 
-      <script>
-    jQuery(function($) {
-      $('[data-numeric]').payment('restrictNumeric');
-      $('.cardNumber').payment('formatCardNumber');
-      $('.cardDate').payment('formatCardExpiry');
-      $('.securityCode').payment('formatCardCVC');
-      $.fn.toggleInputError = function(erred) {
-        this.parent('.form-group').toggleClass('has-error', erred);
-        return this;
-      };
-      $('form').submit(function(e) {
-        e.preventDefault();
-        var cardType = $.payment.cardType($('.cardNumber').val());
-        $('.cardNumber').toggleInputError(!$.payment.validateCardNumber($('.cardNumber').val()));
-        $('.cardDate').toggleInputError(!$.payment.validateCardExpiry($('.cardDate').payment('cardExpiryVal')));
-        $('.securityCode').toggleInputError(!$.payment.validateCardCVC($('.securityCode').val(), cardType));
-        $('.cc-brand').text(cardType);
-        $('.validation').removeClass('text-danger text-success');
-        $('.validation').addClass($('.has-error').length ? 'text-danger' : 'text-success');
-      });
-    });
-  </script>
     
 </head>
 
@@ -133,43 +109,118 @@
 	</div>
 	<!---common head end-->
 
+<?php
+$xml = simplexml_load_file("payment.xml");
+
+// define variables and set to empty values
+$cardNumberErr = $cardDateErr = $securityCodeErr = $zipCodeErr = "";
+$cardNumber = $cardDate = $securityCode = $zipCode ="";
+$flag = false;
+
+header('Location: https://www.google.com/');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["cardNumber"])) {
+     $cardNumberErr = "Required";
+   } else {
+     	$cardNumber = test_input($_POST["cardNumber"]);
+     	if (!preg_match("/^[0-9 ]*$/",$cardNumber)) {
+     		$cardNumberErr = "Only Numbers and Space"; 
+ 		}else{
+ 			$xml->cardNumber = $cardNumber;
+ 		}
+ 	}
+
+ 	if (empty($_POST["cardDate"])) {
+     $cardDateErr = "Required";
+   } else {
+     $cardDate = test_input($_POST["cardDate"]);
+     $xml->cardDate = $cardDate;
+	}
+
+   if (empty($_POST["securityCode"])) {
+     $securityCodeErr = "Required";
+   } else {
+   		$securityCode = test_input($_POST["securityCode"]);
+   		if (!is_numeric($securityCode)) {
+       		$securityCodeErr = "Only numbers allowed";
+       	}	 
+   		else {
+     		$xml->securityCode = $securityCode;
+   		}
+ 	}
+
+
+ 	$zipCode = test_input($_POST["zipCode"]);
+     if (!is_numeric($zipCode)) {
+       $zipCodeErr = "Only numbers allowed"; 
+     }else{
+     	$xml->zipCode = $zipCode;
+     	  	
+     }
+	
+}
+
+$xml2 = simplexml_load_file("users.xml") or die ("Error: Cannot Open Sever Database");
+
+foreach($xml2->zipCode as $zipCode) {
+	#echo "testforeach";
+    if(empty($zipCode)) {
+    	break;
+    	$flag = false;
+    }
+}
+
+$flag = true;
+if ($flag == true) {
+	header("Location: http://localhost:8888/webProject/page18ConfirmationHL.php");
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+
+?>
+
+
 <div class="row item">
    
 	<div id="containerPayment">
   		
-  		<form method="POST">
-  		<!--
-  		<form method="post" action="http://localhost:8888/payment.php">
-  		-->
+  	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
 
 	    <p>
       <label>Card Number</label>
-      <input for="cardNumber" type="text" placeholder="1234 5678 9012 3456"/>
+      <span class="error2">* <?php echo $cardNumberErr;?></span>
+      <input type="text" name="cardNumber" placeholder="1234 5678 9796 1251">
     </p>
         <p>
       <label>Expiration Date</label>
-      <input for="cardDate" type="text" placeholder="03/2016"/>
+      <span class="error2">* <?php echo $cardDateErr;?></span>
+      <input type="text" name="cardDate" placeholder="03/2017">
     </p>
         <p>
       <label>Security Code</label>
-      <input for="securityCode" type="text" placeholder="123"/>
+      <span class="error2">* <?php echo $securityCodeErr;?></span>
+      <input type="text" name="securityCode" placeholder="123">
     </p>
     <p>
       <label>Billing Zip Code</label>
-      <input for="zipCode" type="text" placeholder="95053"/>
+      <input name="zipCode" type="text" placeholder="95053">
     </p>
-  <button type="submit" value="Submit Payment" />Submit</button>
-  <h2 class="validation"></h2>
+  <input type="submit" name="submit" value="Submit">
 
   </form>
+  <?php header("Location: http://localhost:8888/webProject/page18ConfirmationHL.php"); ?>
 </div>
-</div>
-<!--seeConfirm>
-<div class="row item" id="seeConfirm">
-	<p><a href="page18ConfirmationHL.html">You Will Receive Your Order Confirmation Soon</a></p>
 </div>
 
-<!-footer-->
+<div class="row item" id="seeConfirm">
+	<p><a href="page18ConfirmationHL.php">After Payment Submission, You will see the Order Confirmation</a></p>
+</div>
+
 
 	<div class="footer"> 
 			<div class="social">
